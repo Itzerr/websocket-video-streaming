e9900f102a37030703d0899ea3d65cbec842422e
Itzerr <eatzer@gmail.com>
2022-09-03 12:37:45 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# websocket-video-streaming
+## Installation
+```
+git clone https://github.com/Itzerr/websocket-video-streaming.git
+cd websocket-video-streaming
+npm install
+npm run start
+```
---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import { AddressInfo, WebSocketServer } from 'ws'
+import fs from 'fs'
+import path from 'path'
+import express from 'express'
+
+const app = express();
+
+app.use(express.static('public'));
+
+const expressServer = app.listen(3000, () => {
+  const address = expressServer.address() as AddressInfo;
+  console.log(`express listening on port ${address.port}`);
+})
+
+const kibibyte = 1024;
+const mebibyte = 1024 * 1024;
+const defaultChunkSize = mebibyte * 0.1;
+
+const server = new WebSocketServer({ port: 3001 }, () => {
+  const address = server.address() as AddressInfo;
+  console.log(`websocketserver listening on port ${address.port}`);
+});
+
+// function getMp4Codecs(filePath: string): [any] {
+//   const buffer = fs.readFileSync(filePath);
+//   return muxjs.mp4.probe.tracks(buffer)
+// }
+
+function readFileChunk(filePath: string, position: number, chunkSize: number) {
+  const fileSize = fs.statSync(filePath).size;
+  if (position > (fileSize - 1)) {
+    return null;
+  }
+  if (position + chunkSize > fileSize) {
+    chunkSize = fileSize - position;
+  }
+  const buffer = Buffer.allocUnsafe(chunkSize);
+  const fd = fs.openSync(filePath, 'r');
+  fs.readSync(fd, buffer, { length: chunkSize, offset: 0, position: position })
+  fs.closeSync(fd);
+
+  return buffer;
+}
+
+server.on('connection', async (client, req) => {
+  console.log(`connection established with ${(req.socket.address() as AddressInfo).address}`);
+
+  client.on('message', async (data: string) => {
+    const msg = JSON.parse(data);
+
+    switch (msg.type) {
+      case 'video': {
+        if (!msg.filePath) {
+          break;
+        }
+        const filePath = path.resolve('./assets/videos/', msg.filePath);
+        const start = msg.start ?? 0;
+        const size = msg.size ?? defaultChunkSize;
+
+        const buffer = readFileChunk(filePath, start, size);
+        if (!buffer) {
+          break;
+        }
+        const data = buffer.toJSON().data;
+
+        client.send(JSON.stringify({
+          type: 'video',
+          filePath: msg.filePath,
+          data: data
+        }));
+        break;
+      }
+      // case 'codecs': {
+      //   const filePath = path.resolve('./assets/videos/', msg.filePath);
+      //   const codecs = getMp4Codecs(filePath);
+      //   client.send(JSON.stringify({
+      //     type: 'codecs',
+      //     filePath: msg.filePath,
+      //     codecs: codecs
+      //   }));
+      //   break;
+      // }
+    }
+  });
+});
